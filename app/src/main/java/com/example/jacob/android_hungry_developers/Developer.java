1f2a1781eba9e7c5efae88282ad68f749d6046c0
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-10 13:22:56 -0800

    Add data to main activity to test.

--- app/src/main/java/com/example/jacob/android_hungry_developers/Developer.java
+++ app/src/main/java/com/example/jacob/android_hungry_developers/Developer.java
@@ -1,10 +1,14 @@
 package com.example.jacob.android_hungry_developers;
 
+import android.util.Log;
+
 public class Developer implements Runnable {
+    private int id;
     private Spoon leftSpoon;
     private Spoon rightSpoon;
 
-    public Developer(Spoon leftSpoon, Spoon rightSpoon) {
+    public Developer(int id, Spoon leftSpoon, Spoon rightSpoon) {
+        this.id = id;
         this.leftSpoon = leftSpoon;
         this.rightSpoon = rightSpoon;
     }
@@ -12,11 +16,18 @@ public class Developer implements Runnable {
     public void eat() {
         leftSpoon.pickUp();
         rightSpoon.pickUp();
+        Log.i("DevActivity",String.format("Dev %s is eating.",id));
+        try {
+            Thread.sleep((long) (Math.random()*2000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void think() {
         leftSpoon.putDown();
         rightSpoon.putDown();
+        Log.i("DevActivity",String.format("Dev %s is thinking.",id));
     }
 
 
--- app/src/main/java/com/example/jacob/android_hungry_developers/MainActivity.java
+++ app/src/main/java/com/example/jacob/android_hungry_developers/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.jacob.android_hungry_developers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spoon spoon1 = new Spoon();
+        Spoon spoon2 = new Spoon();
+        Spoon spoon3 = new Spoon();
+        Spoon spoon4 = new Spoon();
+        Spoon spoon5 = new Spoon();
+
+        Developer dev1 = new Developer(1, spoon5, spoon1);
+        new Thread(dev1).start();
+        Developer dev2 = new Developer(1, spoon1, spoon2);
+        new Thread(dev2).start();
+        Developer dev3 = new Developer(1, spoon2, spoon3);
+        new Thread(dev3).start();
+        Developer dev4 = new Developer(1, spoon3, spoon4);
+        new Thread(dev4).start();
+        Developer dev5 = new Developer(1, spoon4, spoon5);
+        new Thread(dev5).start();
     }
 }

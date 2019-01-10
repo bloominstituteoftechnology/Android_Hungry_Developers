04e586f17a017ae4102b6f63f84ba95794d559c9
ThaDocIzN <rodan72@msn.com>
2019-01-10 17:37:18 -0600

    part 3 done

---
+++ Concurrency/app/src/main/java/com/thadocizn/concurrency/Developer.java
@@ -0,0 +1,26 @@
+package com.thadocizn.concurrency;
+
+public class Developer {
+
+    private int id;
+    private Spoon leftSpoon;
+    private Spoon rightSpoon;
+
+    public Developer(int id, Spoon leftSpoon, Spoon rightSpoon) {
+        this.id = id;
+        this.leftSpoon = leftSpoon;
+        this.rightSpoon = rightSpoon;
+    }
+
+    public void think(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void run(){
+
+    }
+}

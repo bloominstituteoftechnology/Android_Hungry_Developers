0c142e90d688d15bfcb60df6e456af74bf682858
JTyzz <jtyzzer@gmail.com>
2019-01-10 16:08:44 -0800

    Created Spoon and Developer classes and necessary methods

---
+++ app/src/main/java/com/meterstoinches/earthdefensesystem/android_hungry_developers/Developer.java
@@ -0,0 +1,24 @@
+package com.meterstoinches.earthdefensesystem.android_hungry_developers;
+
+public class Developer implements Runnable{
+    private int id;
+    private Spoon left, right;
+
+    public Developer(int id, Spoon left, Spoon right) {
+        this.id = id;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void eat(){
+
+    }
+
+    public void think(){
+
+    }
+
+    public void run(){
+
+    }
+}
